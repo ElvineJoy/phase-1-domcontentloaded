d0b80baface0c18f50d2966201937b5eb5372e92
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-30 11:29:27 +0300

    Dom Content Loaded lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   const textEditor = document.getElementById("text");
+    textEditor.textContent ="This is really cool!"
+    document.body.appendChild(textEditor);
+  });
